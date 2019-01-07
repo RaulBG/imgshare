0fe4708d22c2a15ec91caf252c5731b68284d6a0
Raul Gomez <raul_nouni@hotmail.com>
2019-01-06 21:28:34 -0600

    Subiendo imagenes al servidor

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,21 @@
-const ctrl = {}
+const ctrl             = {}
+const path             = require('path')
+const { randomNumber } = require('../helpers/libs')
+const fs               = require('fs-extra')
 
 ctrl.index = (req, res) => {
     
 }
 
-ctrl.create = (req, res) => {
-
+ctrl.create = async (req, res) => {
+    const imgName    = randomNumber()
+    const temp       = req.file.path
+    const ext        = path.extname(req.file.originalname).toLowerCase()
+    const targetPath = path.resolve(`src/public/upload/${imgName}${ext}`)
+    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        await fs.rename(temp, targetPath)
+    }
+    res.send('It works!')
 }
 
 ctrl.like = (req, res) => {
