a5ef69090a0668cc9163c4b47258633a92b512d1
Raul Gomez <raul_nouni@hotmail.com>
2019-01-13 20:34:39 -0600

    Guardando informacion de img en MongoDB

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,20 +2,41 @@ const ctrl             = {}
 const path             = require('path')
 const { randomNumber } = require('../helpers/libs')
 const fs               = require('fs-extra')
+const { Image }        = require('../models')
 
 ctrl.index = (req, res) => {
     
 }
 
-ctrl.create = async (req, res) => {
-    const imgName    = randomNumber()
-    const temp       = req.file.path
-    const ext        = path.extname(req.file.originalname).toLowerCase()
-    const targetPath = path.resolve(`src/public/upload/${imgName}${ext}`)
-    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-        await fs.rename(temp, targetPath)
+ctrl.create = (req, res) => {
+    const saveImage = async () => {
+        const imgName = randomNumber()
+        const images     = await Image.find({ filaname: imgName})
+        if (images.length > 0) {
+            saveImage()
+        } else {
+            const temp       = req.file.path
+            const ext        = path.extname(req.file.originalname).toLowerCase()
+            const targetPath = path.resolve(`src/public/upload/${imgName}${ext}`)
+            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+                await fs.rename(temp, targetPath)
+                const newImg = new Image({
+                    title: req.body.title,
+                    filename: imgName + ext,
+                    description: req.body.description
+                })
+                const imageSaved = await newImg.save()
+                // res.redirect('/images')
+                res.send('works')
+            } else {
+                await fs.unlink(temp)
+                res.status(500).json({
+                    error: 'Only images are allowed'
+                })
+            }
+        }
     }
-    res.send('It works!')
+    saveImage()
 }
 
 ctrl.like = (req, res) => {
