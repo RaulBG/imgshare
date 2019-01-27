246428261cb4ac3325104449e01750c46f23a059
Raul Gomez <raul_nouni@hotmail.com>
2019-01-26 19:05:55 -0600

    Eliminar imagenes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,3 +5,23 @@ $('#btn-like').click(function (e) {
         $('.likes-count').text(data.likes)
     })
 })
+$('#btn-delete').click(function (e) {
+    e.preventDefault()
+    var $this = $(this)
+    var confirmar = confirm('Are you sure?')
+    if (confirmar) {
+        var image_id = $this.data('id')
+        $.ajax({
+            url: '/images/' + image_id,
+            type: 'DELETE',
+        }).done(function (response) {
+            $this.removeClass('btn-danger').addClass('btn-success')
+            $this.html('<span>Deleted</span>')
+        })
+    }
+})
+$('.show').hide()
+$('#btn-toggle-comment').click(function (e) {
+    e.preventDefault()
+    $('.show').slideToggle()
+})
